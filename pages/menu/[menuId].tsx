d05331403d4cdf51106809775627dd3e5fc08fa9
MASA-JAPAN <masaifactoryline@gmail.com>
2020-07-21 22:17:46 +0900

    [Add] create menu page for end user

--- pages/menu/[menuId].tsx
+++ pages/menu/[menuId].tsx
@@ -1,39 +1,22 @@
-//TODO: change file's name
-
 import React from "react";
 
 //AppBar
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 import { Theme, createStyles, makeStyles } from "@material-ui/core/styles";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import IconButton from "@material-ui/core/IconButton";
-import InfoIcon from "@material-ui/icons/Info";
 
 import Container from "@material-ui/core/Container";
 
 import { firestore, auth } from "../../utils/firebaseInit";
 
-import AddCircleIcon from "@material-ui/icons/AddCircle";
-
-import { spacing } from "@material-ui/system";
 import Box from "@material-ui/core/Box";
 
-import FormDialog from "../../components/FormDialog";
-import EditDialog from "../../components/EditDialog";
 import DetailDialog from "../../components/DetailDialog";
-
-import EditIcon from "@material-ui/icons/Edit";
-
-import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser";
-
-import Router from "next/router";
 
 import getConfig from "next/config";
 const { publicRuntimeConfig } = getConfig();
@@ -59,9 +42,9 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-function Edit(props: any) {
+function Menu(props: any) {
   const classes = useStyles();
-  console.log(props.id);
+  console.log(props.menuId);
 
   const [tileData, setTileData] = React.useState<object[]>([{ id: null }]);
   const [dataId, setDataId] = React.useState<string>("");
@@ -76,25 +59,15 @@ function Edit(props: any) {
 
     setDataId(tmpDataId);
 
-    // const queries = { userDataId: "JKgKxkYy5PmJn1b4PXwe", menuId: "testmenu" };
-    // axios
-    //   .get("/api/foods", { params: queries })
-    //   .then((res: any) => {
-    //     console.log(res.data[0].id);
-    //   })
-    //   .catch((error: any) => {
-    //     console.log(error, queries);
-    //   });
-
     console.log(tmpDataId);
-    console.log(props.id);
+    console.log(props.menuId);
 
     setDataRef(
       firestore
         .collection("users")
         .doc(tmpDataId)
         .collection("menus")
-        .doc(props.id)
+        .doc(props.menuId)
         .collection("foods")
     );
 
@@ -105,7 +78,7 @@ function Edit(props: any) {
         .collection("users")
         .doc(tmpDataId)
         .collection("menus")
-        .doc(props.id)
+        .doc(props.menuId)
         .collection("foods")
         .get()
         .then((docs: any) => {
@@ -132,17 +105,7 @@ function Edit(props: any) {
     <div className="container">
       <AppBar position="fixed">
         <Toolbar>
-          <Typography>QR WEB MENU</Typography>
-          <Box position="absolute" right={0}>
-            <IconButton
-              color="secondary"
-              aria-label="open"
-              component="span"
-              onClick={() => window.open(`/menu/${props.id}`, "_blank")}
-            >
-              <OpenInBrowserIcon />
-            </IconButton>
-          </Box>
+          <Typography>MENU</Typography>
         </Toolbar>
       </AppBar>
 
@@ -152,10 +115,6 @@ function Edit(props: any) {
             <GridList cellHeight={180} className={classes.gridList}>
               {tileData.map((tile: any) => (
                 <GridListTile key={tile.id}>
-                  <Box position="absolute" right={2} top={2} zIndex={100}>
-                    <EditDialog tile={tile} dataRef={dataRef} />
-                  </Box>
-
                   <img src={tile.img} alt={tile.title} />
                   <GridListTileBar
                     title={tile.title}
@@ -165,9 +124,6 @@ function Edit(props: any) {
               ))}
             </GridList>
           </div>
-          <Box position="fixed" right={2} bottom={2}>
-            <FormDialog id={props.id} dataId={dataId} />
-          </Box>
         </Container>
       </Box>
 
@@ -182,12 +138,8 @@ function Edit(props: any) {
   );
 }
 
-Edit.getInitialProps = async ({ query }: { query: any }) => {
-  const id = query?.id;
-  console.log(id);
-  console.log(query);
-
-  return { id: query?.id };
+Menu.getInitialProps = async ({ query }: { query: any }) => {
+  return { menuId: query?.menuId };
 };
 
-export default Edit;
+export default Menu;
